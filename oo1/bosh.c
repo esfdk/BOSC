1b2dbc32e526c516e184e87e262eef1e0c57de7b
root <root@bosc.imm.dtu.dk>
2012-09-21 12:52:02 +0200

    first version of bosc handin1

---
+++ oo1/bosh.c
@@ -0,0 +1,73 @@
+/* bosh.c : BOSC shell */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/utsname.h> // For the uname() function
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "parser.h"
+#include "print.h"
+
+/* --- symbolic constants --- */
+#define HOSTNAMEMAX 100
+
+/* --- use the /proc filesystem to obtain the hostname --- */
+char *gethostname(char *hostname)
+{
+  FILE *hostnamefile;
+  char hname[HOSTNAMEMAX];
+  char line[HOSTNAMEMAX];
+
+  hostnamefile = fopen ("/proc/sys/kernel/hostname", "r");
+  
+  while (fgets(line, HOSTNAMEMAX, hostnamefile))
+  {
+    if(sscanf(line, "%s", hname)) hostname = hname;
+  }
+
+  return hostname;
+}
+
+/* --- execute a shell command --- */
+int executeshellcmd (Shellcmd *shellcmd)
+{
+  printshellcmd(shellcmd);
+
+  return 0;
+}
+
+/* --- main loop of the simple shell --- */
+int main(int argc, char* argv[]) {
+
+  /* initialize the shell */
+  char *cmdline;
+  char hostname[HOSTNAMEMAX];
+  int terminate = 0;
+  Shellcmd shellcmd;
+  
+  char *newhostname;
+
+  if (newhostname = gethostname(hostname)) {
+
+    /* parse commands until exit or ctrl-c */
+    while (!terminate) {
+      printf("%s", newhostname);
+      if (cmdline = readline(":# ")) {
+	if(*cmdline) {
+	  add_history(cmdline);
+	  if (parsecommand(cmdline, &shellcmd)) {
+	    terminate = executeshellcmd(&shellcmd);
+	  }
+	}
+	free(cmdline);
+      } else terminate = 1;
+    }
+    printf("Exiting bosh.\n");
+  }    
+    
+  return EXIT_SUCCESS;
+}
+
