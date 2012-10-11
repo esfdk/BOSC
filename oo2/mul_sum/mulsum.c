bec8f0604f6c275e3b9887daa246b6e59f35ca98
root <root@bosc.imm.dtu.dk>
2012-10-11 13:31:28 +0200

    got inits working

--- oo2/mul_sum/mulsum.c
+++ oo2/mul_sum/mulsum.c
@@ -7,9 +7,9 @@
 typedef struct calc_struct calc;
 
 struct calc_struct {
-	double minimum_number;
-	double maximum_number;
-	double sumsqrt;
+	float minimum_number;
+	float maximum_number;
+	float sumsqrt;
 };
 
 void *TaskCode(void *argument)
@@ -24,23 +24,26 @@ void *TaskCode(void *argument)
 int main(int argc, char *argv[])
 {
 	int number_of_threads = 8;
-	printf("Number of threads init.");
-	int input_integer = atoi(argv[1]);
-	printf("Input integer calculated.");
+	int input_integer = atoi(argv[2]);
+
 	calc calc_result[number_of_threads];
 	pthread_t threads[number_of_threads];
-	double work = input_integer / number_of_threads;
-		
-	
+
+	float input_float = (float) input_integer;
+	float work = input_float / (float) number_of_threads;	
+
 	int n;
 	for(n = 0; n < number_of_threads; ++n)
 	{
-		calc_result[n].minimum_number = floor((work * n) + 1);
-		calc_result[n].maximum_number = floor(work * (n + 1));
+		
+		float minnum = floor(work * n) + 1;
+		float maxnum = floor(work * (n + 1));
+		calc_result[n].minimum_number = minnum;
+		calc_result[n].maximum_number = maxnum;
 		calc_result[n].sumsqrt = 0;	
-		printf("min %d max %d", calc_result[n].minimum_number, calc_result[n].maximum_number);
+		printf("min %f max %f \n", calc_result[n].minimum_number, calc_result[n].maximum_number);
 	}
-	
+
 	int rc, i;
 
 	/* create all threads */
